d1862dc9a7aeef54ae31599f6e126aae2035396d
antonmes <antonmes@users.noreply.github.com>
2016-03-19 00:37:32 +0300

    #include openssl.h hack

---
+++ openssl.h
@@ -0,0 +1,14 @@
+#ifndef openssl_h
+#define openssl_h
+
+#import <openssl/evp.h>
+#import <openssl/e_os2.h>
+#import <openssl/rand.h>
+#import <openssl/err.h>
+#import <openssl/crypto.h>
+#import <openssl/ssl.h>
+#import <openssl/x509v3.h>
+#import <openssl/bn.h>
+#import <openssl/srtp.h>
+
+#endif
