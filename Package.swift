3cff9730c9c168f7868ff58d3046a45492520ccf
Paulo Faria <paulo.faria.rl@gmail.com>
2016-08-19 19:31:11 -0300

    remove useless directories

--- Package.swift
+++ Package.swift
@@ -1,10 +1,5 @@
 import PackageDescription
 
 let package = Package(
-	name: "COpenSSL",
-	pkgConfig: "openssl",
-	providers: [
-		.Brew("openssl"), 
-		.Apt("libssl-dev")
-	]
+	name: "COpenSSL"
 )
---
+++ Sources/include/openssl.h
@@ -0,0 +1,6 @@
+
+#include "bio.h"
+#include "err.h"
+#include "ssl.h"
+#include "sha.h"
+#include "x509.h"
