ba1e737256818557143dd3fb24a66f58eda189aa
Paulo Faria <paulo.faria.rl@gmail.com>
2017-05-23 20:49:59 -0300

    go back to system package

---
+++ openssl.h
@@ -0,0 +1,22 @@
+#ifndef __ZEWO_OPENSSL_H__
+#define __ZEWO_OPENSSL_H__
+
+#include <openssl/evp.h>
+#include <openssl/e_os2.h>
+#include <openssl/rand.h>
+#include <openssl/err.h>
+#include <openssl/crypto.h>
+#include <openssl/md5.h>
+#include <openssl/ssl.h>
+#include <openssl/x509v3.h>
+#include <openssl/bn.h>
+#include <openssl/srtp.h>
+#include <openssl/conf.h>
+#include <openssl/bio.h>
+#include <openssl/md4.h>
+#include <openssl/sha.h>
+#include <openssl/hmac.h>
+#include <openssl/ripemd.h>
+#include <openssl/pkcs12.h>
+
+#endif
